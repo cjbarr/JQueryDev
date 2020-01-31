6389bf81a3fc149c1e7d53aabee36a98b6d4ee62
Corey Barr <corey.jbarr@gmail.com>
2020-01-31 11:14:41 -0600

    inputs working

--- script.js
+++ script.js
@@ -6,29 +6,44 @@ let songs = [];
 //creation of songs array
 
 function onReady(){
-    $('#updateSongsButton').on('click',displaySongs);
+    $('#updateSongsButton').on('click',addSong);
     
 }//end onReady
 
+function addSong(){
+//select inputs by ID
+//use data in a new song object
+    let newSong = {artist: $('#addArtist').val(),
+        title: $('#addTitle').val(),
+        album: $('#addAlbum').val(),
+        year: $('#addYear').val(),
+            }//end newSong
+//push new song object to songs array
+songs.push(newSong);
+//display songs
+displaySongs();
+
+}//end addSong
 
 function displaySongs(){
     console.log('in displaySongs');
 //target output element by ID
-let el = $('#songsOut')
+let el = $('#songsOut');
 //empty output element
 el.empty();
 //loop through songs array
-//for(let i=0;i<songs.length;i++)
+for(let i=0;i<songs.length;i++){
 // for ofs don't work in every language but standard for loops work across languages
-for( item of songs){
+// for( item of songs){
 //append each song to DOM within loop
-//el.append(`<li> ${songs[i].title} </li >`)
-    el.append(`<li> ${item.title}: ${item.artist}, ${item.album}, (${item.year}) </li>`)
+    el.append(`<li> ${songs[i].title}: ${songs[i].artist}, ${songs[i].album}, (${songs[i].year}) </li>` )
+    
 
 }
-}//end displaySongs
-
+//end displaySongs
+}
 //test song
+
 songs.push({artist:'Red Hot Chili Peppers',
 title: 'Under the Bridge',
 album:'Blood Sugar Sex Magik',
