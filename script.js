a22f068070bb9b87d36b1bccd1c367b96dac5414
Corey Barr <corey.jbarr@gmail.com>
2020-01-31 11:58:36 -0600

    toggle is working

--- script.js
+++ script.js
@@ -7,23 +7,46 @@ let songs = [];
 
 function onReady(){
     $('#updateSongsButton').on('click',addSong);
-    
+    $('#songsOut').on('click','.dopeButton', dopeSong)
 }//end onReady
 
 function addSong(){
 //select inputs by ID
-//use data in a new song object
+//use data in a new song object (using .val as a getter)
     let newSong = {artist: $('#addArtist').val(),
         title: $('#addTitle').val(),
         album: $('#addAlbum').val(),
         year: $('#addYear').val(),
             }//end newSong
+//empty inputs (using .val as a setter)
+$('#addArtist').val('');
+$('#addTitle').val('');
+$('#addAlbum').val('');
+$('#addYear').val('');
 //push new song object to songs array
 songs.push(newSong);
 //display songs
 displaySongs();
 
 }//end addSong
+
+
+function dopeSong(){
+console.log('in dopeSong');
+let el =$(this); //THIS clicked button
+//if dope set to lame, otherwise set to dope
+if (el.text() ==='Dope'){
+    el.text('Lame')
+}//end if 
+else{el.text('Dope')
+}//end else
+//change style of parent
+//select parent element
+let parent = $(this).parent();
+console.log("my parent is", parent)
+parent.toggleClass('lame');
+}//end dopeSong
+
 
 function displaySongs(){
     console.log('in displaySongs');
@@ -36,7 +59,8 @@ for(let i=0;i<songs.length;i++){
 // for ofs don't work in every language but standard for loops work across languages
 // for( item of songs){
 //append each song to DOM within loop
-    el.append(`<li> ${songs[i].title}: ${songs[i].artist}, ${songs[i].album}, (${songs[i].year}) </li>` )
+    el.append(`<li class="dope"> ${songs[i].title}: ${songs[i].artist}, ${songs[i].album}, (${songs[i].year})
+    <button class="dopeButton">Dope</button></li>` )
     
 
 }
