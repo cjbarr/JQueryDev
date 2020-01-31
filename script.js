478d634d050faa39cf33dd226189219761d1ee0a
Corey Barr <corey.jbarr@gmail.com>
2020-01-31 10:34:39 -0600

    button click

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 $(document).ready(onReady);
 
+//songs app
+
+let songs = [];
+//creation of songs array
+
 function onReady(){
+    $('#updateSongsButton').on('click',displaySongs);
+    
 }//end onReady
+
+
+function displaySongs(){
+    console.log('in displaySongs');
+
+}//end displaySongs
+
+//test song
+songs.push({artist:'Red Hot Chili Peppers',
+song: 'Under the Bridge',
+album:'Blood Sugar Sex Magik',
+year: 1991
+})
